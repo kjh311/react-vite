14a35dcd9c45650f24994db4157d3ca420cffe9e
kjh311 <kjh311@gmail.com>
2025-03-09 21:57:05 -0600

    last

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const names = ["Jack", "David", "Kevin", "John"];
 const person = {
   Name: "Kevin",
@@ -14,14 +15,41 @@ const content = `
   <div>
     <h1>Hello World</h1>
     ${Object.entries(person)
-      .map(
-        ([key, value]) => `
-      <div>
-        <p>${key}: ${value}</p>
-      </div>
-    `
-      )
+      .map(([key, value]) => {
+        return `<div>
+  ${
+    value === "Kevin"
+      ? `<h1 class="blue">${key}</h1>`
+      : `<h1 class="red">${key}</h1>`
+  }
+  <p>${value}</p>
+</div>`;
+      })
       .join("")}
+
+      ${names
+        .map((name) => {
+          return `<div>
+        ${
+          name === "David"
+            ? `<p class="blue">${name}</p>`
+            : `<p class="red">${name}</p>`
+        }
+        </div>`;
+        })
+        .join("")}
+
+        ${nums
+          .map((num) => {
+            return `<div>
+            ${
+              num === 3
+                ? `<span class='blue'>${num}</span>`
+                : `<span class='red'>${num}</span>`
+            }
+          </div>`;
+          })
+          .join("")}
   </div>
 `;
 
